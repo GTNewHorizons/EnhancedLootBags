8229394c29b2e8b4858d6551eda2e048c0c2df00
Namikon <namikon@gmx.net>
2016-03-12 16:41:11 +0100

    Added E-Mail Address in order to verify identity for CodeSign-Certificate at Certum

--- src/main/java/eu/usrv/enhancedlootbags/core/ContainerLootBag.java
+++ src/main/java/eu/usrv/enhancedlootbags/core/ContainerLootBag.java
@@ -1,5 +1,5 @@
 /*
-    Copyright 2016 Stefan 'Namikon' Thomanek
+    Copyright 2016 Stefan 'Namikon' Thomanek <sthomanek at gmail dot com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
